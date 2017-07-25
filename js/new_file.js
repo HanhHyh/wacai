317a281f117c5a7ccccb479eb85d6ecde4c3d334
HanhHyh <710366335@.com>
2017-07-25 23:09:55 +0800

    first commit

---
+++ js/new_file.js
@@ -0,0 +1,19 @@
+var n1, n2, n3, n4;
+var z1, z2;
+
+z1 = document.getElementById("hot1");
+z2 = document.getElementById("hot2")
+n1 = document.getElementById("n1");
+n2 = document.getElementById("n2");
+n3 = document.getElementById("n3");
+n4 = document.getElementById("n4");
+
+function method1() {
+	z1.style.display = "block";
+	z2.style.display = "none";
+}
+
+function method2() {
+	z1.style.display = "none";
+	z2.style.display = "block";
+}
